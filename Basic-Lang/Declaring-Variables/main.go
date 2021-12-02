0a302fb1a0c64a05e8f45b876c21cb4e52b1863e
Samuel Giannotti <samuelgiannotti@hotmail.com>
2021-12-02 19:08:55 -0300

    variable declaration

---
+++ Basic-Lang/Declaring-Variables/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x float64 = 20.0
+
+	y := 42
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Printf("x is of type %T\n", x)
+	fmt.Printf("y is of type %T\n", y)
+
+	var a, b, c = 3, 4, "foo"
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Printf("a is of type %T\n", a)
+	fmt.Printf("b is of type %T\n", b)
+	fmt.Printf("c is of type %T\n", c)
+}
