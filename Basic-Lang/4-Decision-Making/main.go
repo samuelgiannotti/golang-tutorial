9b8b2730c9b88d7867069072ae039bf869abbc94
Samuel Giannotti <samuelgiannotti@hotmail.com>
2021-12-03 09:54:04 -0300

    improving format directive

--- Basic-Lang/4-Decision-Making/main.go
+++ Basic-Lang/4-Decision-Making/main.go
@@ -106,12 +106,12 @@ func selectSample() {
 	var i1, i2 int
 	select {
 	case i1 = <-c1:
-		fmt.Printf("received ", i1, " from c1\n")
+		fmt.Printf("received %d %s", i1, " from c1\n")
 	case c2 <- i2:
-		fmt.Printf("sent ", i2, " to c2\n")
+		fmt.Printf("sent %d %s ", i2, " to c2\n")
 	case i3, ok := (<-c3): // same as: i3, ok := <-c3
 		if ok {
-			fmt.Printf("received ", i3, " from c3\n")
+			fmt.Printf("received  %d %s ", i3, " from c3\n")
 		} else {
 			fmt.Printf("c3 is closed\n")
 		}
