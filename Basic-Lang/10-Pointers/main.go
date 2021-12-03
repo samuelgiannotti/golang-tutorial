9a0812e81287c73f9e977593baf60e28092fb695
Samuel Giannotti <samuelgiannotti@hotmail.com>
2021-12-03 13:11:13 -0300

    Pointers

--- Basic-Lang/10-Pointers/main.go
+++ Basic-Lang/10-Pointers/main.go
@@ -7,6 +7,8 @@ func main() {
 	basicPointer2()
 	pointerNilSample()
 	arrayOfPointers()
+	pointerToPointer()
+	pointerToFunction()
 }
 
 func basicPointer() {
@@ -48,4 +50,48 @@ func arrayOfPointers() {
 	for i = 0; i < MAX; i++ {
 		fmt.Printf("Value of a[%d] = %d\n", i, *ptr[i])
 	}
+}
+
+func pointerToPointer() {
+	fmt.Printf("pointerToPointer \n")
+	var a int
+	var ptr *int
+	var pptr **int
+
+	a = 3000
+	/* take the address of var */
+	ptr = &a
+	/* take the address of ptr using address of operator & */
+	pptr = &ptr
+
+	/* take the value using pptr */
+	fmt.Printf("Value of a = %d\n", a)
+	fmt.Printf("Value available at *ptr = %d\n", *ptr)
+	fmt.Printf("Value available at **pptr = %d\n", **pptr)
+}
+
+func pointerToFunction() {
+	fmt.Printf("pointerToFunction \n")
+	/* local variable definition */
+	var a int = 100
+	var b int = 200
+
+	fmt.Printf("Before swap, value of a : %d\n", a)
+	fmt.Printf("Before swap, value of b : %d\n", b)
+
+	/* calling a function to swap the values.
+	* &a indicates pointer to a ie. address of variable a and
+	* &b indicates pointer to b ie. address of variable b.
+	 */
+	swap(&a, &b)
+
+	fmt.Printf("After swap, value of a : %d\n", a)
+	fmt.Printf("After swap, value of b : %d\n", b)
+}
+
+func swap(x *int, y *int) {
+	var temp int
+	temp = *x /* save the value at address x */
+	*x = *y   /* put y into x */
+	*y = temp /* put temp into y */
 }
