0e92c9b980674253785606608968e45b4d697eb4
Samuel Giannotti <samuelgiannotti@hotmail.com>
2021-12-03 08:42:28 -0300

    including select statement in decision making

--- Basic-Lang/4-Decision-Making/main.go
+++ Basic-Lang/4-Decision-Making/main.go
@@ -7,6 +7,7 @@ func main() {
 	ifElseSample()
 	nestedIfSample()
 	switchSample()
+	selectSample()
 }
 
 func ifSample() {
@@ -97,4 +98,24 @@ func switchSample() {
 		fmt.Printf("Invalid grade\n")
 	}
 	fmt.Printf("Your grade is  %s\n", grade)
+}
+
+func selectSample() {
+	//the type of select statement must be a communication channel
+	var c1, c2, c3 chan int
+	var i1, i2 int
+	select {
+	case i1 = <-c1:
+		fmt.Printf("received ", i1, " from c1\n")
+	case c2 <- i2:
+		fmt.Printf("sent ", i2, " to c2\n")
+	case i3, ok := (<-c3): // same as: i3, ok := <-c3
+		if ok {
+			fmt.Printf("received ", i3, " from c3\n")
+		} else {
+			fmt.Printf("c3 is closed\n")
+		}
+	default:
+		fmt.Printf("no communication\n")
+	}
 }
