eb7917a4fdef059df8d7f4bea8c4c61dbe26341b
Samuel Giannotti <samuelgiannotti@hotmail.com>
2021-12-05 21:57:10 -0300

    Slices

---
+++ Basic-Lang/12-Slices/main.go
@@ -0,0 +1,55 @@
+package main
+
+import "fmt"
+
+func main() {
+	var numbers = make([]int, 3, 5)
+	printSlice(numbers)
+	nilSlice()
+	subslicing()
+}
+
+func printSlice(x []int) {
+	fmt.Printf("len=%d cap=%d slice=%v\n", len(x), cap(x), x)
+}
+
+func nilSlice() {
+	fmt.Printf("nilSlice\n")
+	var numbers []int
+	printSlice(numbers)
+
+	if numbers == nil {
+		fmt.Printf("slice is nil\n")
+	}
+}
+
+func subslicing() {
+	fmt.Printf("subslicing\n")
+	/* create a slice */
+	numbers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	printSlice(numbers)
+
+	/* print the original slice */
+	fmt.Println("numbers ==", numbers)
+
+	/* print the sub slice starting from index 1(included) to index 4(excluded)*/
+	fmt.Println("numbers[1:4] ==", numbers[1:4])
+
+	/* missing lower bound implies 0*/
+	fmt.Println("numbers[:3] ==", numbers[:3])
+
+	/* missing upper bound implies len(s)*/
+	fmt.Println("numbers[4:] ==", numbers[4:])
+
+	numbers1 := make([]int, 0, 5)
+	printSlice(numbers1)
+
+	/* print the sub slice starting from index 0(included) to index 2(excluded) */
+	number2 := numbers[:2]
+	printSlice(number2)
+
+	/* print the sub slice starting from index 2(included) to index 5(excluded) */
+	number3 := numbers[2:5]
+	printSlice(number3)
+
+}
